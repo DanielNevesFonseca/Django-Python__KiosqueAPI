d27d28123f6270e8e4a6739724d576f82ffc172b
Daniel Neves Fonseca <daniel.neves.fonseca.ti@gmail.com>
2023-11-16 20:07:02 -0300

    feat: add calculate_tab logic and tested

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from management import product_handler
+from management import product_handler, tab_handler
 from menu import products
 
 if __name__ == "__main__":
@@ -7,19 +7,26 @@ if __name__ == "__main__":
     # print()
     # print(get_products_by_type("drink"))
     # print()
-    new_product = {
-        "title": "X-Python",
-        "price": 5.0,
-        "rating": 5,
-        "description": "Sanduiche de Python",
-        "type": "fast-food"
-    }
-    new_product1 = {
-        "title": "X-Java",
-        "price": 5.0,
-        "rating": 5,
-        "description": "Sanduiche de Java",
-        "type": "fast-food"
-    }
-    print(product_handler.add_product(products, **new_product))
+    # new_product = {
+    #     "title": "X-Python",
+    #     "price": 5.0,
+    #     "rating": 5,
+    #     "description": "Sanduiche de Python",
+    #     "type": "fast-food"
+    # }
+    # new_product1 = {
+    #     "title": "X-Java",
+    #     "price": 5.0,
+    #     "rating": 5,
+    #     "description": "Sanduiche de Java",
+    #     "type": "fast-food"
+    # }
+    # print(product_handler.add_product(products, **new_product))
+    table_2 = [
+        {"_id": 10, "amount": 3},
+        {"_id": 20, "amount": 2},
+        {"_id": 21, "amount": 5},
+    ]
+    table_1 = [{"_id": 1, "amount": 5}, {"_id": 19, "amount": 5}]
+    print(tab_handler.calculate_tab(table_1))
 
