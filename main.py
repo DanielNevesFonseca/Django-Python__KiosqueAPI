380f4f7ffaa16dcdc06c9e4ee57e99ebbeb9eaac
Daniel Neves Fonseca <daniel.neves.fonseca.ti@gmail.com>
2023-11-16 19:23:28 -0300

    feat: add product logic

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from management.product_handler import get_product_by_id, get_products_by_type
+from management import product_handler
+from menu import products
 
 if __name__ == "__main__":
     # Seus prints de teste aqui
-    print(get_product_by_id(30))
-    print()
-    print(get_products_by_type("drink"))
+    # print(get_product_by_id(30))
+    # print()
+    # print(get_products_by_type("drink"))
+    # print()
+    new_product = {
+        "title": "X-Python",
+        "price": 5.0,
+        "rating": 5,
+        "description": "Sanduiche de Python",
+        "type": "fast-food"
+    }
+    new_product1 = {
+        "title": "X-Java",
+        "price": 5.0,
+        "rating": 5,
+        "description": "Sanduiche de Java",
+        "type": "fast-food"
+    }
+    print(product_handler.add_product(
+        products, new_product, new_product1, new_product1
+        ))
--- management/product_handler.py
+++ management/product_handler.py
@@ -19,3 +19,24 @@ def get_products_by_type(product_type: str):
             same_type_list.append(dict)
 
     return same_type_list
+
+
+def add_product(menu_list: list, *products: dict):
+    greater_id = 0
+    new_products_list = []
+
+    for dict in menu_list:
+        current_id = dict["_id"]
+        if current_id > greater_id:
+            greater_id = current_id
+
+    for dict_product in products:
+        greater_id += 1
+        dict_product.update({"_id": greater_id})
+
+        if len(products) > 1:
+            new_products_list.append(dict_product)
+        else:
+            return dict_product
+
+    return new_products_list
