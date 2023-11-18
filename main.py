04560a544a24281f5f892a17296c5f073a2fd4e8
Daniel Neves Fonseca <daniel.neves.fonseca.ti@gmail.com>
2023-11-18 14:20:20 -0300

    feat: add_product_extra function to project tested successfully

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 from management import product_handler
+from menu import products
 if __name__ == "__main__":
     # Seus prints de teste aqui
-    # print(product_handler.get_products_by_type("fruit"))
-    new_product = {
-        "title": "X-Python",
-        "price": 5.0,
-        "rating": 5,
-        "description": "Sanduiche de Python",
-        "type": "fast-food"
-    }
-    print(product_handler.add_product([], **new_product))
+    
--- management/product_handler.py
+++ management/product_handler.py
@@ -25,14 +25,14 @@ def get_products_by_type(product_type: str):
         for key, value in product_dict.items():
             if key == "type" and value == product_type:
                 same_type_list.append(product_dict)
-    
+
     return same_type_list
 
 
 def add_product(menu_list: list, **product: dict):
     greater_id = 0
     dict_product = product
-    
+
     for dict in menu_list:
         current_id = dict["_id"]
         if current_id > greater_id:
@@ -64,3 +64,29 @@ def menu_report():
     most_common_type = type_count.most_common(1)[0][0]
     log_str = f"Products Count: {total_product_quantity} - Average Price: ${average_price:.1f} - Most Common Type: {most_common_type}"
     return log_str
+
+
+def add_product_extra(menu_list: list, *required_keys: dict, **product: dict):
+    greater_id = 0
+    dict_product = product
+    required_keys_list = list(required_keys)
+    dict_validated = {}
+
+    for key, value in dict_product.items():
+        if key in required_keys_list:
+            dict_validated[key] = value
+            required_keys_list.remove(key)
+
+    if len(required_keys_list) != 0:
+        for key in required_keys_list:
+            raise KeyError(f"field {key} is required")
+
+    for dict in menu_list:
+        current_id = dict["_id"]
+        if current_id > greater_id:
+            greater_id = current_id
+
+    greater_id += 1
+    dict_validated.update({"_id": greater_id})
+    menu_list.append(dict_validated)
+    return dict_validated
